f0f09e4488e5029448bed91060400bf3e4b744fa
Pradeepchaudhary81159 <pradeepchaudhary81159@gmail.com>
2022-04-01 18:46:14 +0530

    Shivgangadivine Contraction Web App

---
+++ app/src/main/java/com/shivganga/divine/MainActivity.java
@@ -0,0 +1,455 @@
+package com.shivganga.divine;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.print.PageRange;
+import android.print.PrintAttributes;
+import android.print.PrintDocumentAdapter;
+import android.print.PrintDocumentInfo;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private String URL = "https://shivgangadivine.com/";
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RelativeLayout relativeLayout;
+    private Button reloadBtn;
+    private long backPressedTime;
+    private Toast backToast;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static String file_type = "*/*";
+    private String cam_file_data = null;
+    private ValueCallback<Uri> file_data;
+    private ValueCallback<Uri[]> file_path;
+    private final static int file_req_code = 1;
+
+    private boolean multiple_files = true;
+
+    private ProgressBar progressBar;
+    private PrintAttributes printAttributes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomerwebViewClient client = new CustomerwebViewClient(this);
+
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        relativeLayout = findViewById(R.id.relative_layout);
+        reloadBtn = findViewById(R.id.reload_btn);
+
+        webView = findViewById(R.id.web_view);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.setWebViewClient(client);
+
+        InternetCheck();
+
+        webView.loadUrl(URL);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        });
+
+        reloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(getIntent());
+            }
+        });
+
+//progressbar
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            // page loading progress, gone when fully loaded
+            public void onProgressChanged(WebView view, int progress) {
+
+
+                if (progress < 100 && progressBar.getVisibility() == ProgressBar.GONE) {
+                    progressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+
+                if (progress == 100) {
+                    progressBar.setVisibility(ProgressBar.GONE);
+                }
+                progressBar.setProgress(progress);
+
+
+            }
+
+            /*-- handling input[type="file"] requests for android API 21+ --*/
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+
+                if(file_permission() && Build.VERSION.SDK_INT >= 21) {
+                    file_path = filePathCallback;
+                    Intent takePictureIntent = null;
+                    Intent takeVideoIntent = null;
+
+                    boolean includeVideo = false;
+                    boolean includePhoto = false;
+
+                    /*-- checking the accept parameter to determine which intent(s) to include --*/
+
+                    paramCheck:
+                    for (String acceptTypes : fileChooserParams.getAcceptTypes()) {
+                        String[] splitTypes = acceptTypes.split(", ?+");
+                        /*-- although it's an array, it still seems to be the whole value; split it out into chunks so that we can detect multiple values --*/
+                        for (String acceptType : splitTypes) {
+                            switch (acceptType) {
+                                case "*/*":
+                                    includePhoto = true;
+                                    includeVideo = true;
+                                    break paramCheck;
+                                case "image/*":
+                                    includePhoto = true;
+                                    break;
+                                case "video/*":
+                                    includeVideo = true;
+                                    break;
+                            }
+                        }
+                    }
+
+                    if (fileChooserParams.getAcceptTypes().length == 0) {
+
+                        /*-- no `accept` parameter was specified, allow both photo and video --*/
+
+                        includePhoto = true;
+                        includeVideo = true;
+                    }
+
+                    if (includePhoto) {
+                        takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        if (takePictureIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+                            File photoFile = null;
+                            try {
+                                photoFile = create_image();
+                                takePictureIntent.putExtra("PhotoPath", cam_file_data);
+                            } catch (IOException ex) {
+                                Log.e(TAG, "Image file creation failed", ex);
+                            }
+                            if (photoFile != null) {
+                                cam_file_data = "file:" + photoFile.getAbsolutePath();
+                                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                            } else {
+                                cam_file_data = null;
+                                takePictureIntent = null;
+                            }
+                        }
+                    }
+
+                    if (includeVideo) {
+                        takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                        if (takeVideoIntent.resolveActivity(MainActivity.this.getPackageManager()) != null) {
+                            File videoFile = null;
+                            try {
+                                videoFile = create_video();
+                            } catch (IOException ex) {
+                                Log.e(TAG, "Video file creation failed", ex);
+                            }
+                            if (videoFile != null) {
+                                cam_file_data = "file:" + videoFile.getAbsolutePath();
+                                takeVideoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(videoFile));
+                            } else {
+                                cam_file_data = null;
+                                takeVideoIntent = null;
+                            }
+                        }
+                    }
+
+                    Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                    contentSelectionIntent.setType(file_type);
+
+                    if (multiple_files) {
+                        contentSelectionIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                    }
+
+
+                    Intent[] intentArray;
+                    if (takePictureIntent != null && takeVideoIntent != null) {
+                        intentArray = new Intent[]{takePictureIntent, takeVideoIntent};
+                    } else if (takePictureIntent != null) {
+                        intentArray = new Intent[]{takePictureIntent};
+                    } else if (takeVideoIntent != null) {
+                        intentArray = new Intent[]{takeVideoIntent};
+                    } else {
+                        intentArray = new Intent[0];
+                    }
+
+                    Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                    chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                    chooserIntent.putExtra(Intent.EXTRA_TITLE, "File chooser");
+                    chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+                    startActivityForResult(chooserIntent, file_req_code);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+
+        });
+        //External storage permission for saving file
+
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+
+                Log.d("permission","permission denied to WRITE_EXTERNAL_STORAGE - requesting it");
+                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions,1);
+            }
+
+        }
+        //handle downloading
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setMimeType(mimeType);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie",cookies);
+                request.addRequestHeader("User-Agent",userAgent);
+                request.setDescription("Downloading file....");
+                request.setTitle(URLUtil.guessFileName(url,contentDisposition,mimeType));
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,URLUtil.guessFileName(url, contentDisposition, mimeType));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(),"Downloading File",Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(Build.VERSION.SDK_INT >= 21){
+            Uri[] results = null;
+
+            /*-- if file request cancelled; exited camera. we need to send null value to make future attempts workable --*/
+            if (resultCode == Activity.RESULT_CANCELED) {
+                file_path.onReceiveValue(null);
+                return;
+            }
+
+            /*-- continue if response is positive --*/
+            if(resultCode== Activity.RESULT_OK){
+                if(null == file_path){
+                    return;
+                }
+                ClipData clipData;
+                String stringData;
+
+                try {
+                    clipData = intent.getClipData();
+                    stringData = intent.getDataString();
+                }catch (Exception e){
+                    clipData = null;
+                    stringData = null;
+                }
+                if (clipData == null && stringData == null && cam_file_data != null) {
+                    results = new Uri[]{Uri.parse(cam_file_data)};
+                }else{
+                    if (clipData != null) {
+                        final int numSelectedFiles = clipData.getItemCount();
+                        results = new Uri[numSelectedFiles];
+                        for (int i = 0; i < clipData.getItemCount(); i++) {
+                            results[i] = clipData.getItemAt(i).getUri();
+                        }
+                    } else {
+                        try {
+                            Bitmap cam_photo = (Bitmap) intent.getExtras().get("data");
+                            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                            cam_photo.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+                            stringData = MediaStore.Images.Media.insertImage(this.getContentResolver(), cam_photo, null, null);
+                        }catch (Exception ignored){}
+
+                        results = new Uri[]{Uri.parse(stringData)};
+                    }
+                }
+            }
+
+            file_path.onReceiveValue(results);
+            file_path = null;
+        }else{
+            if(requestCode == file_req_code){
+                if(null == file_data) return;
+                Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+                file_data.onReceiveValue(result);
+                file_data = null;
+            }
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig){
+        super.onConfigurationChanged(newConfig);
+    }
+
+    public boolean file_permission(){
+        if(Build.VERSION.SDK_INT >=23 && (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 1);
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private File create_image() throws IOException{
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_"+timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName,".jpg",storageDir);
+    }
+
+
+    private File create_video() throws IOException {
+        @SuppressLint("SimpleDateFormat")
+        String file_name    = new SimpleDateFormat("yyyy_mm_ss").format(new Date());
+        String new_name     = "file_"+file_name+"_";
+        File sd_directory   = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(new_name, ".3gp", sd_directory);
+    }
+
+    private void InternetCheck()
+    {
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mobileData = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (mobileData.isConnected())
+        {
+            swipeRefreshLayout.setVisibility(View.VISIBLE);
+            webView.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+        }
+        else if (wifi.isConnected())
+        {
+            swipeRefreshLayout.setVisibility(View.VISIBLE);
+            webView.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+        }
+        else
+        {
+            swipeRefreshLayout.setVisibility(View.GONE);
+            webView.setVisibility(View.GONE);
+            relativeLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
+    class CustomerwebViewClient extends WebViewClient
+    {
+        private Activity activity;
+        CustomerwebViewClient(Activity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            swipeRefreshLayout.setRefreshing(false);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            InternetCheck();
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.contains(URL)) {
+                view.loadUrl(url);
+            } else {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(i);
+            }
+            return true;
+        }
+    }
+
+        @Override
+        public boolean onKeyDown(int keyCode, KeyEvent event) {
+            if (keyCode == KeyEvent.KEYCODE_BACK && this.webView.canGoBack())
+            {
+                this.webView.goBack();
+                return true;
+            }
+            return super.onKeyDown(keyCode, event);
+        }
+
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        } else {
+            backToast = Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
+    }
+
+}
+
+
